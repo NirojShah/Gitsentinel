22157602f5f3082b0e3bbd90bb8e1dcda2d9c954
Niroj Shah <nirojshah1998@gmail.com>
2026-08-15 22:28:50 +0530

    Implement user repository
    Added repository methods for creating users, checking email existence, and retrieving user credentials for authentication.
    Integrated Prisma for database operations with selective field retrieval.

--- orchestrator-server/src/module/user-module/user.repository.ts
+++ orchestrator-server/src/module/user-module/user.repository.ts
@@ -5,61 +5,37 @@ import CustomError from "../../constants/CustomError.js";
 import StatusCode from "../../constants/StatusCode.js";
 
 class UserRepository {
-    async createuser(userBody: Prisma.UserCreateInput) {
-        const userExists = await prisma.user.findFirst({
-            where: {
-                email: userBody.email
-            },
-            select: {
-                email: true
-            }
-        })
 
-        if (userExists) {
-            throw new CustomError(StatusCode.BAD_REQUEST, "User already present please Login")
-        }
-        prisma.user.create({
+    async createuser(userBody: Prisma.UserCreateInput) {
+        return await prisma.user.create({
             data: userBody
         })
-
-        return {
-            status: StatusCode.CREATED,
-            message: "User is created successfully."
-        }
     }
 
-    async loginUser(loginCredential: UserType.Login) {
+    async userExistsByEmail(email: string) {
+        return await prisma.user.findFirst({
+            where: {
+                email: email
+            },
+            select: {
+                email: true,
+                name: true
+            }
+        })
+    }
 
+    async authenticateUser(email: string) {
+        return await prisma.user.findFirst({
+            where: {
+                email,
+            },
+            select: {
+                email: true,
+                password: true,
+                id: true
+            }
+        })
     }
 }
 
 export default UserRepository;
-
-
-
-// async loginUser(loginCredential: UserType.Login) {
-//     const user = await this.userRepository.findByEmail(
-//         loginCredential.email
-//     );
-
-//     if (!user) {
-//         throw new CustomError(
-//             StatusCode.UNAUTHORIZED,
-//             "Invalid email or password"
-//         );
-//     }
-
-//     const isPasswordValid = await bcrypt.compare(
-//         loginCredential.password,
-//         user.password
-//     );
-
-//     if (!isPasswordValid) {
-//         throw new CustomError(
-//             StatusCode.UNAUTHORIZED,
-//             "Invalid email or password"
-//         );
-//     }
-
-//     return user;
-// }
