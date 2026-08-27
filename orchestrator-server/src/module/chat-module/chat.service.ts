e37d3e2f42ce8599dc093deda5494ea6d760cfe5
Niroj Shah <nirojshah1998@gmail.com>
2026-08-27 23:40:51 +0530

    feat(chat): implement chat creation service

--- orchestrator-server/src/module/chat-module/chat.service.ts
+++ orchestrator-server/src/module/chat-module/chat.service.ts
@@ -1,11 +1,48 @@
 import type ResponseDto from "../../constants/ResponseDto.js";
+import StatusCode from "../../constants/StatusCode.js";
 import type ChatModel from "./chat.model.js";
+import ChatRepository from "./chat.repository.js";
 
 interface ChatService {
-    createChat(data: ChatModel.createChat): Promise<ResponseDto>;
+    createChat(data: ChatModel.CreateChat): Promise<ResponseDto>;
     findChatById(id: string): Promise<ResponseDto>;
-    updateChat(id: string, data: ChatModel.updateChat): Promise<ResponseDto>;
+    updateChat(
+        id: string,
+        data: ChatModel.UpdateChat
+    ): Promise<ResponseDto>;
 }
 
+class ChatServiceImpl implements ChatService {
+    private readonly chatRepository = new ChatRepository();
+    async createChat(data: ChatModel.CreateChat): Promise<ResponseDto> {
+        const isChatExists: boolean = await this.chatRepository.chatExistsByData(data);
+        if (isChatExists) {
+            return {
+                statusCode: StatusCode.BAD_REQUEST,
+                message: "The chat already exists."
+            }
+        }
 
-export default ChatService;
+        const resp = await this.chatRepository.save(data);
+
+        return {
+            statusCode: StatusCode.CREATED,
+            message:"successfully chat created.",
+            data: resp
+        }
+
+    }
+
+    findChatById(id: string): Promise<ResponseDto> {
+        throw new Error("Method not implemented.");
+    }
+
+    updateChat(
+        id: string,
+        data: ChatModel.UpdateChat
+    ): Promise<ResponseDto> {
+        throw new Error("Method not implemented.");
+    }
+}
+
+export default ChatServiceImpl;
