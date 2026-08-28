dd46ce08d5896bdad7a0a72a1d1999fbe726a575
Niroj Shah <nirojshah1998@gmail.com>
2026-08-28 17:58:07 +0530

    Add chat creation controller validation
    Implement chat creation handling in ChatController by validating
    request data with CreateChatSchema, returning a bad request error
    for invalid input, and delegating valid data to the chat service.

---
+++ orchestrator-server/src/module/chat-module/chat.controller.ts
@@ -0,0 +1,42 @@
+import type { Request, Response } from "express";
+import type ChatServiceImpl from "./chat.service.js";
+import type AuthenticatedRequest from "../../constants/AuthenticatedRequest.type.js";
+import CustomError from "../../constants/CustomError.js";
+import ChatModel from "./chat.model.js";
+import StatusCode from "../../constants/StatusCode.js";
+
+class ChatController {
+    constructor(private readonly chatService: ChatServiceImpl) {
+    }
+    async createChat(req: AuthenticatedRequest, res: Response): Promise<Response> {
+        const result = ChatModel.CreateChatSchema.safeParse(req.body)
+
+        if (!result.success) {
+            throw new CustomError(
+                StatusCode.BAD_REQUEST,
+                result.error.message
+            );
+        }
+
+        const response = await this.chatService.createChat(result.data);
+
+        return res.status(response.statusCode).json({
+            data: response.data,
+            message: response.message
+        })
+    }
+
+    async updateChat(req: AuthenticatedRequest, res: Response): Promise<Response> {
+        throw new Error("not implemented.")
+    }
+
+    async findChat(req: AuthenticatedRequest, res: Response): Promise<Response> {
+        throw new Error("not implemented.")
+    }
+
+    async findMyChat(req: AuthenticatedRequest, res: Response): Promise<Response> {
+        throw new Error("not implemented.")
+    }
+}
+
+export default ChatController;
